d5d627dbcfd5c8ed42112db4f875b437725eea17
rtm-goeun <goeun@rtm.ai>
2024-06-11 22:42:28 +0900

    [Feat] inital setting

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -1,5 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
+// Place Photos
+// function createPhotoMarker(place) {
+//   var photos = place.photos;
+//   if (!photos) {
+//     return;
+//   }
+
+//   var marker = new google.maps.Marker({
+//     map: map,
+//     position: place.geometry.location,
+//     title: place.name,
+//     icon: photos[0].getUrl({maxWidth: 35, maxHeight: 35})
+//   });
+// }
+
 const Marker: React.FC<google.maps.MarkerOptions> = ({ ...options }) => {
   const [marker, setMarker] = useState<google.maps.Marker>();
 
